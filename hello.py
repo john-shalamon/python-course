8659e687300c3a65cb5a297b1b03c37349dad80b
john-shalamon <johnshalamon.cs22@stellamaryscoe.edu.in>
2025-04-01 19:24:48 +0530

    first commit

---
+++ hello.py
@@ -0,0 +1,10 @@
+line1='*******************************'
+line2='                           '
+line3=' welcome to python world ' 
+line4='                           '                      
+line5='********************************'  
+print(line1)
+print(line2)
+print(line3)
+print(line4)
+print(line5)                         
