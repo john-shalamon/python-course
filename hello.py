8649bbbdc632a0f3bc1e6f2cc903f9fe6923db52
john-shalamon <johnshalamon.cs22@stellamaryscoe.edu.in>
2025-04-11 00:09:07 +0530

    Add additional print statements and string casting demonstration in hello.py

--- hello.py
+++ hello.py
@@ -28,3 +28,10 @@ print(h1+h2+h3)
 print("hello world")
 print("hello world")            
 print("hello world")
+
+#casting a nummber to str
+decaDE=str(10)
+print(decaDE)
+print(type(decaDE))
+statement="i am "+ decaDE+""+"years old"
+print(statement)
